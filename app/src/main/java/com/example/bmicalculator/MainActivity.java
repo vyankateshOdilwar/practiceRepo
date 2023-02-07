219ad883a8cb27898abf1357d0593624f21e1da2
vodilwar03 <vodilwar@gmail.com>
2023-02-07 18:41:16 +0530

    without setting onClickListner

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -49,12 +49,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerContactAdapter adapter = new recyclerContactAdapter(this, arrContactsList);
         recyclerView.setAdapter(adapter);
 
-        onCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
-            }
-        });
 
      }
 
